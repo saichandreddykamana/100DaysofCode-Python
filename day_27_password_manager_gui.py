597fe6addb66529294935e138d3e0c773bef6e6a
Sai Chand Reddy Kamana <saichand.kamana@outlook.com>
2022-12-14 12:24:26 +0000

    Added Exception handling and search feature
    Added Exception handling and search feature

--- day_27_password_manager_gui.py
+++ day_27_password_manager_gui.py
@@ -1,8 +1,8 @@
-import tkinter.messagebox
 from tkinter import *
-import random
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 from tkinter import messagebox
+import random
+import json
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
 def password_generator():
@@ -39,20 +39,53 @@ def password_generator():
 
 
 def save_data():
-    website_data = website_entry.get()
+    website_data = website_entry.get().capitalize()
     email_data = email_entry.get()
     password_data = password_entry.get()
     if website_data != "" and email_data != "" and password_data != "":
         permission = messagebox.askokcancel(title=website_data, message=f"{email_data} is email correct? \n {password_data} is password correct? \n Is it okay to save?")
         if permission:
-            entry_data = f"{website_data} | {email_data} | {password_data}\n"
-            with open('data.txt', 'a') as file:
-                file.write(entry_data)
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
-            website_entry.focus()
+            new_data = {
+                website_data: {
+                    'email': email_data,
+                    'password': password_data
+                }
+            }
+            try:
+                with open('data.json', 'r') as file:
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open('data.json', 'w') as file:
+                    json.dump(new_data, file, indent=4)
+            else:
+                data.update(new_data)
+                with open('data.json', 'w') as file:
+                    json.dump(data, file, indent=4)
+            finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+                website_entry.focus()
     else:
         messagebox.showerror(title="Empty Fields", message="One or more fields are empty")
+
+# ---------------------------- SEARCH DATA ------------------------------- #
+
+
+def search_data():
+    data_to_search = website_entry.get()
+    try:
+        data = {}
+        with open('data.json', 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        with open('data.json', 'w') as file:
+            json.dump(data, file, indent=4)
+    finally:
+        if len(data) == 0 or data_to_search.capitalize() not in data:
+            messagebox.showerror(title="No entry found", message="No entry found")
+        else:
+            messagebox.showinfo(title="Entry Found", message=f" Email: {data[data_to_search.capitalize()]['email']} \n Password: {data[data_to_search.capitalize()]['password']}")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -76,8 +109,8 @@ email_label.grid(row=2, column=0)
 password_label = Label(width=15, text="Password", font=("Calibri", 11, "bold"), background="white")
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=50, font=("Calibri", 11, "bold"))
-website_entry.grid(row=1, column=1, columnspan=2, padx=5, pady=5)
+website_entry = Entry(width=35, font=("Calibri", 11, "bold"))
+website_entry.grid(row=1, column=1, columnspan=1, padx=5, pady=5)
 website_entry.config(border="2px solid black", borderwidth=2)
 website_entry.focus()
 
@@ -89,6 +122,10 @@ email_entry.insert(0, 'sai9318@gmail.com')
 password_entry = Entry(width=34, font=("Calibri", 11, "bold"))
 password_entry.grid(row=3, column=1, padx=5, pady=5)
 password_entry.config(border="2px solid black", borderwidth=2)
+
+search_button = Button(text="Search", width=10)
+search_button.config(background="#5897EE", foreground="white", command=search_data)
+search_button.grid(row=1, column=2)
 
 generate_password_button = Button(text="Generate Password")
 generate_password_button.config(background="#5897EE", foreground="white", command=password_generator)
